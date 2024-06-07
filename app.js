5efbc26eef67ae06e4ad0392dc9f69d121ce45b5
lualuaSantosS2 <luana.santos@sptech.school>
2024-06-07 11:04:41 -0300

    apagando alterações

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
+// var medidasRouter = require("./src/routes/medidas");
+// var aquariosRouter = require("./src/routes/aquarios");
+// var empresasRouter = require("./src/routes/empresas");
 var consultaRouter = require("./src/routes/consulta");
 var medicacaoRouter = require('./src/routes/medicacao');
 
@@ -31,9 +31,9 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
+// app.use("/medidas", medidasRouter);
+// app.use("/aquarios", aquariosRouter);
+// app.use("/empresas", empresasRouter);
 app.use("/consulta", consultaRouter);
 app.use("/medicacao", medicacaoRouter);
 
